19ce7b6d14e7e1725759bc0060783bd23fda816f
Moray <mrnabar@gmail.com>
2020-11-30 13:46:13 +0530

    Adding some comments

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-1. Create a random three part message each time the program is run
-2. Output will be funny fortune cookie messages
-3. Part 1 - 
-    Will be about an action in the present
-4. Part 2 - 
-    Will be about a tragic event in the future
-5. Part 3 - 
-    Will tell a way of avoiding the future event
-6. Create a repository on GitHub
-7. Messages will be stored in an object
-8. The object will have 3 array properties coressponding to the three parts of the message
-9. Select each part of the message at random using the random method of Math object
-10. Length of each array will be the same
-11. Entire message will be saved as a String literal
-*/
-
 // Object to store all the possible messages
 const mixedMessage = {
   adjective: [
@@ -50,8 +33,8 @@ const randomMessagePart = (property) =>
 // Function to join the three parts together
 const completeMessage = () => {
   const finalMessage = [];
-  const keys = Object.keys(mixedMessage);
-  keys.forEach((key) => {
+  const keys = Object.keys(mixedMessage); // Collect all keys of the object
+  keys.forEach((key) => {  // Iterate through the properties of the object
     switch (key) {
       case "adjective":
         finalMessage.push(`Beware a ${randomMessagePart(key)}`);
